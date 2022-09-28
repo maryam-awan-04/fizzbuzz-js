9f9a712e74b2aaa6948b4581587c3b884781904a
Maryam Awan <maryamawan04@gmail.com>
2022-09-28 14:48:11 +0100

    fizz printed if number is divisible by 3, buzz printed if number is divisible by 5, fizzbuzz printed if number is divisible by both

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,8 +7,19 @@ function fizzbuzz() {
 
     var i;
 
-    for (i=0; i<=100; i++) {
-        console.log(i);
+    for (i = 3; i <= 100; i++) {
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+            console.log("fizzbuzz")
+        }
+        else if (i % 3 == 0) {
+            console.log("fizz");
+        }
+        else if (i % 5 == 0) {
+            console.log("buzz")
+        }
+        else {
+            console.log(i);
+        }
     }
 
     
