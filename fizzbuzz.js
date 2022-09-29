e3021adf19afc9e892bc3208de17c33c69e4a1fe
Maryam Awan <maryamawan04@gmail.com>
2022-09-29 13:49:39 +0100

    fezz printed when number is divisible by 13, fezz is always at the beginning

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,12 +3,23 @@
 // Here, we create our main function.
 function fizzbuzz() {
     
-    for (let i = 1; i <= 110; i++) {
+    for (let i = 1; i <= 200; i++) {
 
-        let word = ['fizz', 'buzz', 'bang', 'bong']
+        let word = ['fizz', 'buzz', 'bang', 'bong', 'fezz']
         let output = ''
 
+        // divisisble by 11
         if (i % 11 === 0) {output = word[3]}
+
+        // divisible by 13
+        else if (i % 13 === 0){
+            if ((i % 3 === 0) && (i % 5 === 0)) {output = [word[4],word[0],word[1]].join('')}
+            else if (i % 3 === 0) {output = [word[4],word[0]].join('')}
+            else if (i % 5 === 0) {output = [word[4],word[1]].join('')}
+            else if (i % 7 === 0) {output = [word[4],word[2]].join('')}
+            else if (i % 11 === 0) {output = [word[4],word[3]].join('')}
+            else {output = word[4]}
+        }
 
         // divisible by 7
         else if (i % 7 === 0){
